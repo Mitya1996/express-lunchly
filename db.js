cd0cc3bd27f611ee614d7627bae23bfe9a529579
mits <dmitrybaycharov@gmail.com>
2021-07-08 09:49:21 -0500

    nodemon listening for html files and Part Five: Full Names finished

--- db.js
+++ db.js
@@ -2,7 +2,9 @@
 
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client(
+  "postgresql://postgres:myPassword@localhost:5432/lunchly"
+);
 
 db.connect();
 
